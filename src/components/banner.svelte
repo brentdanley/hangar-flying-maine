12fc13400dca4852d89aa835eb57a36af34f2a7f
Brent Danley <brentdanley@gmail.com>
2023-11-26 23:55:17 -0500

    Adds event announcement banner.

---
+++ src/components/banner.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+	import { differenceInDays } from 'date-fns';
+
+	const nextEventDate = new Date('2023-11-30'); // Replace with the date of the next event
+	const daysUntilNextEvent = differenceInDays(nextEventDate, new Date()) + 1;
+</script>
+
+{#if daysUntilNextEvent > 0}
+	<div class="banner">
+		The next Hangar Flying Maine <a href="/2023-11-30_take-two">event</a> is in {daysUntilNextEvent}
+		{daysUntilNextEvent === 1 ? 'day' : 'days'}.
+	</div>
+{/if}
+
+<style lang="scss">
+	.banner {
+		background-color: var(--tertiary-color);
+		color: white;
+		padding: 1rem;
+		text-align: center;
+		border-radius: 0.5rem;
+
+		a {
+			font-weight: bold;
+			color: inherit;
+		}
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,20 @@
 <script lang="ts">
 	import * as config from '$lib/config';
+	import Banner from '../components/banner.svelte';
 </script>
 
 <svelte:head>
 	<title>{config.title}</title>
 </svelte:head>
 
+<Banner />
+
 <h1>Welcome!</h1>
 
 <p>Hangar Flying Maine is a casual monthly social event for aviation enthusiasts.</p>
+
+<p><a href="/signup">Sign up</a> for the mailing list to be notified of future events.</p>
+
+<p>See a list of upcoming and past <a href="/events">local aviation events</a>.</p>
+
+<p>Read about the <a href="/history">rich history</a> of Hangar Flying Maine.</p>
