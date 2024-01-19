6c2ada7ca92109182096b8d420d753d4677cd106
Brent Danley <brentdanley@gmail.com>
2024-01-19 17:16:42 -0500

    Changes name of header and footer files

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
 	import Navigation from '../components/MobileNavigation.svelte';
-	import Header from '../components/Header.svelte';
-	import Footer from '../components/Footer.svelte';
+	import Header from '../components/H.svelte';
+	import Footer from '../components/F.svelte';
 	import '../app.scss';
 	import Banner from '../components/banner.svelte';
 </script>
